460c769d20c61559715f17115b06d2cd8377edca
Marcel Mravec <marcel.mravec@gmail.com>
2022-09-24 21:59:05 +0200

    ...

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,13 +5,37 @@
 //  Created by Marcel Mravec on 04.09.2022.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
-
+    
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            Button("Select image") {
+                showingImagePicker = true
+            }
+        }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker(image: $inputImage)
+        }
+        .onChange(of: inputImage) { _ in loadImage() }
+        
     }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
